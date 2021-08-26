e35eec43f372a84f77785042f8f20d6318ad8161
simon <swagasoft@gmail.com>
2021-08-26 10:57:58 +0100

    data cleaning-5

--- data-cleaning-5.sql
+++ data-cleaning-5.sql
@@ -3,3 +3,16 @@ SELECT RIGHT(website, 3) AS domain, COUNT(*) num_companies
 FROM accounts
 GROUP BY 1
 ORDER BY 2 DESC;
+
+
+SELECT LEFT(UPPER(name), 1) AS first_letter, COUNT(*) num_companies
+FROM accounts
+GROUP BY 1
+ORDER BY 2 DESC;
+
+SELECT SUM(num) nums, SUM(letter) letters
+FROM (SELECT name, CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
+                       THEN 1 ELSE 0 END AS num, 
+         CASE WHEN LEFT(UPPER(name), 1) IN ('0','1','2','3','4','5','6','7','8','9') 
+                       THEN 0 ELSE 1 END AS letter
+      FROM accounts) t1;
